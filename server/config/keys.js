8423421d7a1ea74456171399da26dc1a9987ee3e
Viraj Shinde <virajabhayshinde@gmail.com>
2024-10-02 14:26:54 +0530

    Second Commit

---
+++ server/config/keys.js
@@ -0,0 +1,11 @@
+//keys.js - figure out what set of credenttials to return
+
+if(process.env.NODE_ENV === 'production'){
+    //we are in production, return the prod set of keys
+    module.exports = require('./prod')
+
+} else{
+    // we are in development return the dev keys
+
+    module.exports = require('./dev')
+}
--- server/index.js
+++ server/index.js
@@ -1,15 +1,36 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+require('./models/User')
+require('./services/passport')
 
+
+
+mongoose.connect(keys.mongoURI)
 const app = express()
 
 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys:[keys.cookieKey]
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 
 app.get('/',(req,res) => {
     res.send("Server Working")
 })
 
 
-app.listen(5000, (err) => {
+const port = process.env.PORT || 5000
+app.listen(port, (err) => {
     if(err){
         console.log(err)
     }else{
@@ -17,4 +38,3 @@ app.listen(5000, (err) => {
     }
 })
 
-//https://github.com/virajshinde
